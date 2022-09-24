45beff63cd329829453d0d72a221f8b0296c4762
Knakong <roathana.kong@gmail.com>
2022-09-23 22:59:47 -0600

    Finished Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    //Solution below
+    String name = "Roathana Kong";
+    int age = 33;
+    String state = "Colorado";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
