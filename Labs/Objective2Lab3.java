79e8f49935274b22606d5f566577f197c02a791d
Knakong <roathana.kong@gmail.com>
2022-09-23 23:13:24 -0600

    Finished Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3{
+  public static void main(String[] args) {
+    //Solution Below
+    int birthYear = 1989;
+    String birthMonth = ("November");
+    boolean weekend = false;
+    System.out.println(birthYear);
+    System.out.println(birthMonth);
+    System.out.println(weekend);
+  }
+}
