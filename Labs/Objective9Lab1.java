f133ed8867b07cdac176f5b5fdad3063d7e9c479
Knakong <roathana.kong@gmail.com>
2022-10-07 04:12:06 -0600

    Finished Objective9Lab1.java

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,16 @@
+public class Objective9Lab1{
+  public static void main(String[] args) {
+
+    printName(); //calls printName() method
+
+  }
+
+  public static void printName(){ //new method printName()
+
+
+    System.out.println("Hello Doug"); //prints Hello Doug
+
+
+  }
+
+}
