090c4b6d10b88f4cea652f4afb778c976832b9f7
Knakong <roathana.kong@gmail.com>
2022-10-07 04:35:46 -0600

    Finished Objective9Lab2

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,10 @@
+public class Objective9Lab2{
+  public static void main(String[] args) {
+    System.out.println("In main"); //prints In Main
+    printMessage(); //calls printMesage()
+    System.out.println("In main"); //prints In Main
+  }
+  public static void printMessage(){ //printMessage method
+    System.out.println("In method"); //prints In Method
+  }
+}
