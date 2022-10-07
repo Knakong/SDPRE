b369be3feffd707d0ad8a7547ce255284702114a
Knakong <roathana.kong@gmail.com>
2022-10-07 00:57:04 -0600

    Finished Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,16 @@
+public class Objective7Lab4{
+  public static void main(String[] args) {
+    int count = 1;
+    int sum = 1;
+
+    while (count < 20){ //while loops when counter is under 20
+
+      count++; //adds 1 to count
+
+      sum +=count; //adds the current value of count to some
+    }
+  System.out.println(sum);
+  //prints out the final sum after while loop
+
+  }
+}
