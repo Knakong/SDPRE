9a1b9fa9edba1897b141f7542ee362c20ecc446f
Knakong <roathana.kong@gmail.com>
2022-10-06 23:03:35 -0600

    Finished Objective7Lab2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,11 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;
+
+    while(counter <= 20) {
+      //will perform body code if value of counter is less than or equal to 20
+      System.out.println(counter); //prints the value of counter
+      counter++; //adds 1 to counter
+    }
+  }
+}
