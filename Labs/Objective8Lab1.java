c5cb2f63edbd26f0d775b15fa383b4bbf87cf961
Knakong <roathana.kong@gmail.com>
2022-10-07 03:02:48 -0600

    FinishedObjective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,18 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    int i;
+
+      //counts 1 to 10
+    for (i = 1; i < 11;  i++){
+      System.out.println(i);
+    }
+
+      //counts backwards 10 to 1
+    for (i = 10; i > 0; i--){
+
+      System.out.println(i);
+    }
+
+  }
+}
